ea5f9fde9ba4e1fea48d2a45057f750ace2e670b
jahedhasan <jaahedhasan.bs@gmail.com>
2022-08-27 21:35:57 +0600

    updated

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+"""
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-
+"""
 
 class Welcome(Screen):
     pass
