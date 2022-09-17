c46e87a2ffd9b9589317089841b6252dba4f27cb
jahedhasan <jaahedhasan.bs@gmail.com>
2022-09-17 12:33:38 +0600

    added list on displayallemployee screen

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from kivy.app import App
+#from kivy.app import App
+from kivymd.app import MDApp
 from kivy.core.window import Window
-from kivy.lang import Builder
+from kivymd.icon_definitions import md_icons
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.list import TwoLineListItem
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.expansionpanel import MDExpansionPanel
+from kivymd.uix.scrollview import MDScrollView
+from kivy.lang import Builder
+
 """
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
@@ -28,6 +35,24 @@ class Login(Screen ):
 class Menu(Screen):
     pass
 
+class MyContent(MDBoxLayout):
+    pass
+class DisplayAllEmployee(Screen):
+    pass
+    '''
+    def on_start(self):
+        names= ["option 1", "option 2", "option 3"]
+        
+        for name in names:
+            
+            panel = MDExpansionPanel(title=name,icon="img/face.jpg",content= MyContent())
+            
+            self.manager.get_screen('displayallemployee').ids.panel_container.add_widget(panel)
+            
+     '''       
+            
+            
+            
 class AddEmployee(Screen):
     checks = []
     
@@ -68,13 +93,17 @@ class AddEmployee(Screen):
 class WindowManager(ScreenManager):
     pass
 
-kv= Builder.load_file('design.kv')  
-class Eapp(App):
+#kv= Builder.load_file('design.kv')  
+class Eapp(MDApp):
+
     #Window.size=(800, 500)
-    Window.clearcolor = "#ffffff"
+    #Window.clearcolor = "#ffffff"
+    
     def build(self):
+        kv= Builder.load_file('design.kv')  
         return kv
     
+ 
 
 
 if __name__ == '__main__':
